2c821d68f0a10211623a196f06d41e58d0d55a45
Mateus Santana <mateuss.dev@gmail.com>
2020-05-08 13:21:48 -0300

    feat: add next-offline options in next-config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,29 @@
-module.exports = {
-  typescript: {
-    ignoreDevErrors: true
+const withOffline = require('next-offline') //  eslint-disable-line
+
+const nextConfig = {
+  target: 'serverless',
+  transformManifest: (manifest) => ['/'].concat(manifest),
+  generateInDevMode: true,
+  workboxOpts: {
+    swDest: 'public/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 7 * 24 * 60 * 60
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
   }
 }
+
+module.exports = withOffline(nextConfig)
