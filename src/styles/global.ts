e8d9256393873a298e0603c06f453842a8aa4fe7
Mateus Santana <mateuss.dev@gmail.com>
2020-05-09 21:24:28 -0300

    feat: add font face config

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,37 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+  @font-face {
+    font-family: 'Titillium Web', sans-serif;
+    src: url('/static/fonts/TitilliumWeb-Light.ttf');
+    font-weight: 300;
+  }
+
+  @font-face {
+    font-family: 'Titillium Web', sans-serif;
+    src: url('/static/fonts/TitilliumWeb-Regular.ttf');
+    font-weight: 400;
+  }
+
+  @font-face {
+    font-family: 'Titillium Web', sans-serif;
+    src: url('/static/fonts/TitilliumWeb-SemiBold.ttf');
+    font-weight: 600;
+  }
+
+  @font-face {
+    font-family: 'Titillium Web', sans-serif;
+    src: url('/static/fonts/TitilliumWeb-Bold.ttf');
+    font-weight: 700;
+  }
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Titillium Web', sans-serif;
   }
 `
