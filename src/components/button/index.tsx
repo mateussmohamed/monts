96152b0e6315a8c52b7408f00f9a426a8a91b4d8
Mateus Santana <mateuss.dev@gmail.com>
2020-05-15 18:55:27 -0300

    feat: add disabled prop in button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,6 +4,7 @@ import { Button, ButtonProps } from 'rebass/styled-components'
 interface Props extends ButtonProps {
   variant?: string
   loading?: boolean
+  disabled?: boolean
 }
 
 const CustomButton: FC<Props> = ({ children, loading, variant = 'primary', ...props }) => {
