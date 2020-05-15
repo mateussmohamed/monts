5cc1cda245c5b2eb479c12ec00d8de191fdc52a0
Mateus Santana <mateuss.dev@gmail.com>
2020-05-15 19:03:46 -0300

    feat: declare new types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,2 +1,16 @@
 declare module '@rebass/preset'
 declare module 'lodash.merge'
+
+declare type User = {
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+  id?: string
+}
+
+declare type Wallet = {
+  id?: string
+  userId: string
+  balance: number
+}
