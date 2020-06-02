085e5cf892ec1e2ab1c2b23f890376d1c4565df4
Mateus Santana <mateuss.dev@gmail.com>
2020-06-02 20:00:33 -0300

    feat: add signup page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -2,94 +2,73 @@ import React from 'react'
 import { useRouter } from 'next/router'
 import * as yup from 'yup'
 import Link from 'next/link'
-import { useForm, FieldValues } from 'react-hook-form'
-import { Flex, Text } from 'rebass/styled-components'
+import { toast } from 'react-toastify'
+import { FieldValues } from 'react-hook-form'
+import { Flex, Text, Button } from 'rebass/styled-components'
 
-import AuthScreen from '@monts/components/auth-screen'
-
+import AuthScreen from '@monts/components/auth-layout'
+import Form from '@monts/components/form'
 import Field from '@monts/components/field'
-import Button from '@monts/components/button'
 
-import api from '@monts/services/api'
-
-type Inputs = {
-  name: string
-  lastName: string
-  email: string
-  password: string
-}
+import fetcher from '@monts/lib/unfetch'
 
 const signUpSchema = yup.object().shape({
-  firstName: yup.string().required('este campo obrigatório.'),
-  lastName: yup.string().required('este campo obrigatório.'),
-  email: yup.string().email('email invalido.').required('este campo obrigatório.'),
-  password: yup.string().required('este campo obrigatório.')
+  firstName: yup.string().required('este campo é obrigatório..'),
+  lastName: yup.string().required('este campo é obrigatório..'),
+  email: yup.string().email('email inválido.').required('este campo é obrigatório..'),
+  password: yup.string().required('este campo é obrigatório..')
 })
 
 function SignUp(): JSX.Element {
   const router = useRouter()
-  const { register, handleSubmit, errors } = useForm<Inputs>({ validationSchema: signUpSchema })
 
   async function onSubmit(formData: FieldValues): Promise<void> {
     try {
-      const response = await api('/api/user/signup', {
+      const response = await fetcher('/api/user/signup', {
         method: 'POST',
         body: JSON.stringify(formData)
       })
 
-      console.log({ response })
+      if (response.data) {
+        toast.success('User registered')
 
-      router.push('/')
+        router.push('/login')
+      } else {
+        toast.error(response.error)
+      }
     } catch (error) {
-      console.log({ error })
+      toast.error(error.data)
     }
   }
 
   return (
     <AuthScreen title="Criar Conta" image="/static/images/illustration/office.svg">
-      <Flex flexDirection="column" alignItems="center" as="form" onSubmit={handleSubmit(onSubmit)} width={1}>
-        <Field name="firstName" label="Nome" placeholder="Informe seu nome" register={register} errors={errors} />
+      <Form onSubmit={onSubmit} validation={signUpSchema}>
+        <Flex flexDirection="column" alignItems="center" width={1}>
+          <Field name="firstName" label="Nome" placeholder="Informe seu nome" />
 
-        <Field
-          name="lastName"
-          label="Sobrenome"
-          placeholder="Informe seu sobrenome"
-          register={register}
-          errors={errors}
-        />
+          <Field name="lastName" label="Sobrenome" placeholder="Informe seu sobrenome" />
 
-        <Field
-          name="email"
-          label="Email"
-          type="email"
-          placeholder="Informe o seu melhor email"
-          register={register}
-          errors={errors}
-        />
+          <Field name="email" label="Email" type="email" placeholder="Informe o seu melhor email" />
 
-        <Field
-          name="password"
-          label="Senha"
-          type="password"
-          placeholder="Informe uma senha segura"
-          register={register}
-          errors={errors}
-        />
+          <Field name="password" label="Senha" type="password" placeholder="Informe uma senha segura" />
 
-        <Button minWidth={200} mt={2} mb={3} type="submit" disabled={Object.keys(errors).length > 0}>
-          Vamos começar
-        </Button>
+          <Button minWidth={200} mt={2} mb={3} type="submit">
+            Vamos começar
+          </Button>
 
-        <Text as="p" color="midnightBlue" textAlign="center" fontWeight="normal" fontSize={[2, 3]}>
-          Já possui uma conta?{' '}
-          <Link href="/login" passHref>
-            <Text as="a" color="primary" fontWeight="semi" fontSize={[2, 3]}>
-              Login
-            </Text>
-          </Link>
-        </Text>
-      </Flex>
+          <Text as="p" color="midnightBlue" textAlign="center" fontWeight="normal" fontSize={[2, 3]}>
+            Já possui uma conta?{' '}
+            <Link href="/login" passHref>
+              <Text as="a" color="primary" fontWeight="semi" fontSize={[2, 3]}>
+                Login
+              </Text>
+            </Link>
+          </Text>
+        </Flex>
+      </Form>
     </AuthScreen>
   )
 }
+
 export default SignUp
