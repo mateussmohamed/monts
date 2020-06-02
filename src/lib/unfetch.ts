dc3771b0e4b3828809a2094e9b9be2478631bdc0
Mateus Santana <mateuss.dev@gmail.com>
2020-06-02 19:58:03 -0300

    feat: add wrapper over unfecth

---
+++ src/lib/unfetch.ts
@@ -0,0 +1,26 @@
+import unfectch from 'isomorphic-unfetch'
+
+export default async function fetcher(...args: unfectch.IsomorphicRequest) {
+  try {
+    const response = await unfectch(...args)
+
+    // if the server replies, there's always some data in json
+    // if there's a network error, it will throw at the previous line
+    const { data, error } = await response.json()
+
+    if (response.ok) {
+      return data
+    }
+
+    const errorInstance = new Error(response.statusText)
+    errorInstance.response = response
+    errorInstance.data = error
+
+    throw errorInstance
+  } catch (error) {
+    if (!error.data) {
+      error.data = { message: error.message }
+    }
+    throw error
+  }
+}
