65662ee4108ba07698bba7c1a250239f581f9722
Mateus Santana <mateuss.dev@gmail.com>
2020-06-02 16:37:53 -0300

    feat: add user api

--- src/pages/api/user/signup.ts
+++ src/pages/api/user/signup.ts
@@ -1,26 +1,37 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import bcrypt from 'bcrypt'
 
-import prisma from '@monts/libs/prisma'
+import prisma from '@monts/lib/prisma'
+import omit from '@monts/utils/omit'
 
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+async function handle(req: NextApiRequest, res: NextApiResponse): Promise<void> {
   try {
     const body: User = req.body
+
+    const user = await prisma.user.findOne({
+      where: {
+        email: body.email
+      }
+    })
+
+    if (user) {
+      return res.status(409).json({ error: 'Usuário já cadastrado.' })
+    }
 
     const salt = bcrypt.genSaltSync()
     const hash = bcrypt.hashSync(body.password, salt)
 
-    const { password, ...savedUser } = await prisma.user.create({
+    const savedUser = await prisma.user.create({
       data: {
         ...body,
         password: hash
       }
     })
 
-    res.status(200).json(savedUser)
+    res.status(200).json({ data: omit(savedUser, ['password']) })
   } catch (error) {
-    console.log(error)
-
-    res.status(409).json(error)
+    return res.status(409).json({ error: 'Internal Error' })
   }
 }
+
+export default handle
