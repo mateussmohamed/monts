ead5ab05be053af16c38baf4b2716b5d506772cc
Mateus Santana <mateuss.dev@gmail.com>
2020-06-02 19:56:30 -0300

    refactor: remove database

--- src/services/api.ts
+++
@@ -1,7 +0,0 @@
-import unfetch from 'isomorphic-unfetch'
-
-export default (url: string, options: object): Promise<void> =>
-  unfetch(url, {
-    headers: { 'Content-Type': 'application/json' },
-    ...options
-  }).then((r) => r.json())
