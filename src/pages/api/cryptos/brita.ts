af16dc401dc005ac4f09604458c4995016227d49
Mateus Santana <mateuss.dev@gmail.com>
2020-06-03 22:15:32 -0300

    feat: add bitcoin brita

---
+++ src/pages/api/cryptos/brita.ts
@@ -0,0 +1,20 @@
+import { NextApiResponse } from 'next'
+import { format } from 'date-fns'
+
+import { OLINDA_API } from '@monts/constants'
+import fetch from '@monts/lib/fetch'
+import parseOlindaPayload from '@monts/utils/parse-olinda-payload'
+
+async function handle(_req: unknown, res: NextApiResponse): Promise<void> {
+  try {
+    const date = format(new Date(), 'MM-dd-yyyy')
+    const url = `${OLINDA_API}/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='${date}'`
+    const { value } = await fetch(url)
+
+    return res.status(200).json({ data: parseOlindaPayload(value[0]) })
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Error' })
+  }
+}
+
+export default handle
