8693e6113c327c49075c6ba644b98d458248d9e8
Mateus Santana <mateuss.dev@gmail.com>
2020-06-03 22:15:17 -0300

    feat: add bitcoin api

---
+++ src/pages/api/cryptos/bitcoin.ts
@@ -0,0 +1,17 @@
+import { NextApiResponse } from 'next'
+
+import { MERCADO_BITCOIN_API } from '@monts/constants'
+import fetch from '@monts/lib/fetch'
+
+async function handle(_req: unknown, res: NextApiResponse): Promise<void> {
+  try {
+    const url = `${MERCADO_BITCOIN_API}/BTC/ticker`
+    const { ticker } = await fetch(url)
+
+    return res.status(200).json({ data: ticker })
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Error' })
+  }
+}
+
+export default handle
