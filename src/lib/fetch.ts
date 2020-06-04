dc6777284b12e270ace4a54176a1a3df20baa9f6
Mateus Santana <mateuss.dev@gmail.com>
2020-06-03 22:07:22 -0300

    refactor: remove destruction prop in response.json

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -3,17 +3,17 @@ import fetch from 'node-fetch'
 export default async function fetcher(
   url: string,
   options?: Record<string, unknown>
-): Promise<Record<string, Record<string, unknown>>> {
+): Promise<Record<string, Record<string, any>>> {
   try {
     const response = await fetch(url, options)
-    const { data, error } = await response.json()
+    const data = await response.json()
 
     if (response.ok) {
       return data
     }
 
     const errorInstance = new Error(response.statusText)
-    errorInstance.message = error
+    errorInstance.message = data?.error
 
     throw errorInstance
   } catch (error) {
