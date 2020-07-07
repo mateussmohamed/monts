648a3f174b66098d6a1c644d68687e0c0d08fb71
Mateus Santana <mateuss.dev@gmail.com>
2020-07-07 00:11:24 -0300

    feat: add BottomNavigation in wallet page

--- src/pages/wallet/index.tsx
+++ src/pages/wallet/index.tsx
@@ -5,6 +5,8 @@ import { Button } from 'rebass/styled-components'
 import fetch from '@monts/lib/fetch'
 import useUser from '@monts/hooks/use-user'
 
+import BottomNavigation from '@monts/components/bottom-navigation'
+
 function Dashboard(): JSX.Element {
   const { mutateUser } = useUser({ redirectTo: '/login' })
 
@@ -20,6 +22,8 @@ function Dashboard(): JSX.Element {
       <Button mt={4} variant="negative" onClick={handleLogout}>
         Sair
       </Button>
+
+      <BottomNavigation />
     </div>
   )
 }
