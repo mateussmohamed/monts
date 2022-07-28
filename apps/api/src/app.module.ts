f7d3c9b92b7aff552cbb0b74d6289d9a3403fa79
Mateus Santana <mateuss.dev@gmail.com>
2022-07-27 23:46:21 -0300

    chore(api): remove unecessary files

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,32 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { join } from 'path'
+
+import configuration from './common/config/configuration'
+import { upperDirectiveTransformer } from './common/directives/upper-case.directive'
+
+import { UsersModule } from './users/users.module'
+import { WalletsModule } from './wallets/wallets.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      load: [configuration]
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      transformSchema: (schema) => upperDirectiveTransformer(schema, 'upper'),
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class'
+      }
+    }),
+    WalletsModule
+  ]
 })
 export class AppModule {}
