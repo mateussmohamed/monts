270346fc7d1fecae15f4ea0f93156dc2645e4cbe
Mateus Santana <mateuss.dev@gmail.com>
2022-09-11 20:18:12 -0300

    refactor(web): remove api

--- apps/web/src/pages/wallet.tsx
+++ apps/web/src/pages/wallet.tsx
@@ -2,7 +2,7 @@ import fetch from 'domains/shared/lib/helpers/fetch'
 import useUser from 'domains/shared/lib/hooks/use-user'
 import { Button, Heading } from 'rebass/styled-components'
 
-import { BottomNavigation } from '../../ui/components/bottom-navigation'
+import { BottomNavigation } from '../ui/components/bottom-navigation'
 
 const Dashboard = () => {
   const { mutateUser } = useUser({ redirectTo: '/login' })
